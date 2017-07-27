cae4afbe05a5da36eb72c5a91a12289e567c5ada
mcueto <cueto@live.cl>
2017-07-27 00:53:59 -0400

    Add attributes to models

--- producers_api/models.py
+++ producers_api/models.py
@@ -10,9 +10,35 @@ class Producer(models.Model):
         return str(self.contact_email)+" - "+str(self.created_at)
 
 
+class ProductType(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.contact_email)+" - "+str(self.created_at)
+
+
 class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=100)
+    producer = models.ForeignKey(Producer)
+
+    def __str__(self):
+        return str(self.contact_email)+" - "+str(self.created_at)
+
+
+class ProductPresentation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(Product)
+    price = models.FloatField()
+    description = models.TextField()
 
     def __str__(self):
         return str(self.contact_email)+" - "+str(self.created_at)
