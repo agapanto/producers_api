b23ff3b123ddee4ace7bb5eb2e5badf4b02af8c4
mcueto <cueto@live.cl>
2017-09-20 22:42:20 -0300

    Add ProducerType model

--- producers_api/admin.py
+++ producers_api/admin.py
@@ -1,12 +1,14 @@
 """Producers_api admin registration."""
 from django.contrib import admin
 from .models import (
+    ProducerType,
     Producer,
     ProductType,
     Product,
     ProductPresentation,
 )
 
+admin.site.register(ProducerType)
 admin.site.register(Producer)
 admin.site.register(ProductType)
 admin.site.register(Product)
