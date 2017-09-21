fb6897d9b120d49e17f8262617c3e3d116fee01e
mcueto <cueto@live.cl>
2017-09-20 23:29:35 -0300

    Add ProducerStatus model to track status

--- producers_api/admin.py
+++ producers_api/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from .models import (
     ProducerType,
+    ProducerStatus,
     Producer,
     ProductType,
     Product,
@@ -9,6 +10,7 @@ from .models import (
 )
 
 admin.site.register(ProducerType)
+admin.site.register(ProducerStatus)
 admin.site.register(Producer)
 admin.site.register(ProductType)
 admin.site.register(Product)
--- producers_api/models.py
+++ producers_api/models.py
@@ -13,12 +13,23 @@ class ProducerType(models.Model):
         return str(self.name)+" - "+str(self.created_at)
 
 
+class ProducerStatus(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)+" - "+str(self.created_at)
+
+
 class Producer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     name = models.CharField(max_length=100)
     producer_type = models.ForeignKey(ProducerType, blank=True, null=True)
+    status = models.ForeignKey(ProducerStatus)
 
     def __str__(self):
         return str(self.name)+" - "+str(self.created_at)
